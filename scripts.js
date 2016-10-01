12184cdb6b11a4d5ae65b28116d992b0cea4ffc1
Owen Gong <ogong500@millardps.org>
2016-10-01 12:00:16 -0500

    Week 2 Homework

--- scripts.js
+++ scripts.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
-  $.getJSON("https://raw.githubusercontent.com/GETHIP-Classroom/2017-hw2-ogong500/master/owen.json", function(data){
+  $.getJSON("https://github.com/GETHIP-Classroom/2017-hw2-ogong500/blob/master/owen.json", function(data){
     console.log(data);
     $("#profile-name").text(data.firstName + " " + data.lastName);
     $("#age").text(data.age);
     $("#profile-picture").attr("src", data.picture);
     $.each(data.nickNames, function(key, value){
       $("#nicknames").append("<li>" + value.nick + "</li>");
-
+    $.each(data.mixTape, function(key, value){
+      $("#mixtape").append("<li>" + value.mix + "</li>");
     });
   });
 });
